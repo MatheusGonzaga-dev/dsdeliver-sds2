5942c5d2ba732c713dcda984fdf41b3689fafcaf
Matheus Gonzaga <matheusgonzaga77@hotmail.com>
2021-01-06 15:12:42 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Navbar from './Navbar/index';
-
+import Home from './Home/index'
 function App() {
   return (
     <div>
-        <Navbar/>
+        <>
+          <Navbar/>
+          <Home/>
+        </>
     </div>
   );
 }
