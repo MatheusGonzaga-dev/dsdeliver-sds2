e1abc93388741097ad87f02d3e25d4454934f222
Matheus Gonzaga <matheusgonzaga77@hotmail.com>
2021-01-07 15:29:07 -0300

    Adição de imagens e preços

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,24 @@
 import './styles.css';
 import StepsHeader from './StepsHeader'
+import ProductList from './ProductList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
-        
         <div className="orders-container">
             <StepsHeader/>
+            <ProductList products={products}/>
         </div>
     )
 }
