c04230b5128009b96d08f87373f959104f6984e6
Matheus Gonzaga <matheusgonzaga77@hotmail.com>
2021-01-08 14:22:43 -0300

    Serach order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,13 +2,14 @@ import './styles.css';
 import StepsHeader from './StepsHeader'
 import ProductList from './ProductList';
 import { useEffect, useState } from 'react';
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
-    
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
     useEffect(() => {
         fetchProducts()
             .then(response => setProducts(response.data))
@@ -19,6 +20,7 @@ function Orders() {
         <div className="orders-container">
             <StepsHeader/>
             <ProductList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     )
 }
