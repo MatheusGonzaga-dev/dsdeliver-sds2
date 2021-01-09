1f8c751542895690c43756f41abab5293d56adb3
Matheus Gonzaga <matheusgonzaga77@hotmail.com>
2021-01-08 22:55:53 -0300

    finish error

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 const API_URL = process.env.REACT_APP_API_URL;
-const mapboxToken = process.env.REACT_APP_ACESS_TOKEN_MAP_BOX;
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
     return axios(`${API_URL}/products`)
