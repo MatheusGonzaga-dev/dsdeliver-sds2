3dfe9917ea7ad08f8eb45c919f76bd090f36731c
Matheus Gonzaga <matheusgonzaga77@hotmail.com>
2021-01-10 14:01:30 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -3,8 +3,7 @@ import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 
 
@@ -22,8 +21,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home/>
+       <Routes />
     </View>
   );
 }
